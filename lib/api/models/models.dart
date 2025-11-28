dad34ba44987eaa795bf4a04a72400104249b201
manuelduarte077 <carlosd99m@gmail.com>
2025-11-27 18:48:12 -0600

    :construction: initial code

--- lib/api/models/models.dart
+++
@@ -1,1 +0,0 @@
-export 'jobs/job.dart';
--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_graphql_jobs/api/api.dart';
-import 'package:flutter_graphql_jobs/app/app.dart';
-
-void main() {
-  runApp(
-    MyApp(jobsApiClient: JobsApiClient.create()),
-  );
-}
